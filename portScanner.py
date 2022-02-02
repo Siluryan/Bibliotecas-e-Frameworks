36b10571f9674c544f902481ac61f5046a46de6a
Guilherme Dias <80716667+Siluryan@users.noreply.github.com>
2022-02-02 07:58:55 -0300

    Create portScanner.py

---
+++ portScanner.py
@@ -0,0 +1,27 @@
+import socket
+from IPy import IP
+from tqdm import tqdm
+
+ipaddress = input('[+] Enter Target To Scan: ')
+port = 0
+l_open = []
+
+with tqdm(total=1023) as prog_bar:
+
+    while port < 1024:
+        port += 1
+        prog_bar.update(1)
+        try:
+            sock = socket.socket()
+            sock.settimeout(0.02)
+            sock.connect((ipaddress, port))
+            if True:
+                l_open.append(port)
+
+        except:
+            pass
+
+l_open = [str(i) for i in l_open]
+l_open = ", ".join(l_open)
+
+print(f'Open ports: {l_open}')
